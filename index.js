8d0ec4fca2a2e0ec5d18bb60c245583e22e80075
Igor Yegorov <iegoroff@gmail.com>
2019-08-02 20:26:24 +0300

    first

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { MSCMultiSegmentedControl } = NativeModules;
+
+export default MSCMultiSegmentedControl;
---
+++ ios/RNMultiSegmentedControl.m
@@ -0,0 +1,13 @@
+
+#import "RNMultiSegmentedControl.h"
+
+@implementation RNMultiSegmentedControl
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
